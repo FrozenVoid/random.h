8b155e1747ddeb547035ef5d4d660f66fd21f134
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-06-10 14:27:46 +0300

    Update random.h

--- random.h
+++ random.h
@@ -5,7 +5,10 @@ rnd(seed) return randomized seed->rndnum
 rnd1() get random 64bit num
 range(max) ^below max(0-max)
 range32(max) same but for 32-bit(faster)
+//faster variants
 rndbias() faster 64bit RNG(fails PractRand 32 gigabytes)
+rangeb(max)  range with rndbias generator
+range32b(max)   range32 with rndbias generator
 */
 #include <stdint.h>
 static inline uint64_t rotl(const uint64_t x, int k) {
@@ -36,7 +39,17 @@ static inline uint64_t range(uint64_t maxval){
 uint64_t a=rnd1();
 return (((unsigned __int128)a)*((unsigned __int128)maxval))>>64;
 }
+static inline uint64_t rangeb(uint64_t maxval){
+uint64_t a=rndbias();
+return (((unsigned __int128)a)*((unsigned __int128)maxval))>>64;
+}
+
 static inline uint32_t range32(uint32_t maxval){
 uint32_t a=(uint32_t)rnd1();
+return (((uint64_t)a)*((uint64_t)maxval))>>32;
+}
+
+static inline uint32_t range32b(uint32_t maxval){
+uint32_t a=(uint32_t)rndbias();
 return (((uint64_t)a)*((uint64_t)maxval))>>32;
 }
